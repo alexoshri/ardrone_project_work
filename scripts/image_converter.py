image_controller: fix bug in perpendicular intersection calculation

scripts/image_converter.py
@@ -34,8 +34,11 @@ class image_converter:
 
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            #self.image_pub.publish(data)
         except CvBridgeError as e:
             print(e)
+        except:
+            pass
 
         # filtering noise
         kernel = np.ones((5, 5), np.float32) / 25
@@ -94,7 +97,7 @@ class image_converter:
                 h, w = line_mask.shape[:2]
                 line_yx = np.argwhere(thin_line_mask == 255)
                 pt = [h/2, w/2] # <-- the point to find
-                distance, index = spatial.KDTree(line_yx).query(pt,50) # distance always positive
+                _, index = spatial.KDTree(line_yx).query(pt,50) # distance always positive
                 nearest_pt = line_yx[index] # <-- the nearest point to center of frame
                 nearest_pt, _ = kmeans(nearest_pt, 1)
                 nearest_pt = nearest_pt[0]
@@ -121,10 +124,12 @@ class image_converter:
                 # angle between [-90,90]
                 angle = -np.angle((center2_x - center1_x) + (center2_y - center1_y) * 1j, deg=True) - 90  # consider red/blue orientation
                 cv2.line(res1, (center1_x, center1_y), (center2_x, center2_y), (255, 0, 255), 3)
+
+                # calculate nearest point relative to straight line
                 v = np.array([center2_y - center1_y, center2_x - center1_x])
-                P = np.array([h/2 - center1_y,w/2 - center1_y])
+                P = np.array([h/2 - center1_y,w/2 - center1_x])
                 b = float(np.inner(P,v))/float(np.inner(v, v))
-                nearest_pt_on_line = [center1_y,center1_x] +  (b * v).astype(int)
+                nearest_pt_on_line = [center1_y,center1_x] + np.around((b * v)).astype(int)
                 y_nearest_pt_on_line = nearest_pt_on_line[0]
                 x_nearest_pt_on_line = nearest_pt_on_line[1]
                 distance = ((x_nearest_pt_on_line - w / 2) ** 2 + (y_nearest_pt_on_line - h / 2) ** 2) ** 0.5
@@ -146,7 +151,7 @@ class image_converter:
                 #cv2.line(res1, (x_nearest_1, y_nearest + dH / 2), (x_nearest_2, y_nearest - dH / 2), (255, 0, 255), 3)
                 #angle = np.angle(x_nearest_2 - x_nearest_1 + dH * 1j, deg=True) - 90  # consider red/blue orientation
 
-                #cv2.circle(res1, (w / 2, h / 2), 5, (0, 0, 255), -1)
+                cv2.circle(res1, (w / 2, h / 2), 5, (0, 0, 255), -1)
                 #cv2.circle(res1, (x_nearest, y_nearest), 5, (0, 255, 0), -1)
                 cv2.circle(res1, (x_nearest_pt_on_line, y_nearest_pt_on_line), 5, (0, 255, 255), -1)
                 #cv2.circle(res1, (center2_x, center2_y), 5, (0, 255, 0), -1)
@@ -159,7 +164,6 @@ class image_converter:
                 #cv2.putText(res1, 'secs: {}'.format(time_stamp.secs), (w / 2, 150), cv2.FONT_ITALIC, 0.5, (255, 255, 255), 1)
                 #cv2.putText(res1, 'nsecs: {}'.format(time_stamp.nsecs), (w / 2, 180), cv2.FONT_ITALIC, 0.5, (255, 255, 255), 1)
 
-                #self.image_pub.publish(res1)
                 cv2.imshow('res1', res1)
                 # cv2.imshow('res2',res2)
                 # cv2.imshow('thin',thin_line_mask)
@@ -171,8 +175,8 @@ class image_converter:
                 img_calc.distance = distance
 
                 ################################################################################
-                img_calc.arrow_x = x_nearest - w / 2  # x grows from left to right  ###x_nearest
-                img_calc.arrow_y = y_nearest - h / 2  # y grows from top to bottom  ###y_nearest
+                img_calc.arrow_x = x_nearest_pt_on_line - w / 2  # x grows from left to right  ###x_nearest
+                img_calc.arrow_y = y_nearest_pt_on_line - h / 2  # y grows from top to bottom  ###y_nearest
                 img_calc.angle = angle
 
             except:
@@ -184,6 +188,7 @@ class image_converter:
 
         try:
             self.image_pub_calc.publish(img_calc)
+            #self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
         except: # message can arrive before node was initialized
             pass
 